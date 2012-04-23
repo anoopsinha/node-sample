196f6d9703b8145cfbed78bc17495b60c2df5dc5
anoopsinha <anoop.k.sinha@gmail.com>
2012-04-22 17:36:55 -0700

    new string

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('express').createServer();
 app.get('/', function(req, res) {
-	res.send('Hello from Cloud Foundry.  This is an update.  This is another update.');
+	res.send('Hola.  Hello from Cloud Foundry.  This is an update.  This is another update.');
     });
 app.listen(process.env.VCAP_APP_PORT || 3000);
