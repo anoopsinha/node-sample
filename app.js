166a2b6406391179bc059ad56ce8f12255e77e53
Anoop Sinha <anoop.k.sinha@gmail.com>
2012-04-23 10:56:02 -0700

    test phrase

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('express').createServer();
 app.get('/', function(req, res) {
-	res.send('Hello from Cloud Foundry. Hi mom!');
+	res.send('Hello from Cloud Foundry. Hi mom');
     });
 app.listen(process.env.VCAP_APP_PORT || 3000);
